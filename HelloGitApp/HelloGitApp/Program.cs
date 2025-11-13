f4c878f1b3af916ca9d4013f90c5aa69499ea8b9
hafsa703 <hafsa.nnkch@eduge.ch>
2025-11-13 10:45:19 +0100

    Ajout de la saise du nom

---
+++ HelloGitApp/HelloGitApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("Quel est votre nom?");
+            string nom = Console.ReadLine();
+        }
+    }
+}
